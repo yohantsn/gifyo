7405a62916839ee82b9792c875c36d37c9d4b0f2
Yohan Sampaio <31547668+yohantsn@users.noreply.github.com>
2020-04-08 08:39:47 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:gifyo/ui/gif_page.dart';
+import 'package:gifyo/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white,),
+
+  ));
+}
+
+
